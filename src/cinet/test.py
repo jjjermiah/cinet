ee4c2d1ad28268b94ed25eb9b23944b112271836
ChristopherEeles <christopher.eeles@outlook.com>
2022-06-28 20:37:59 +0000

    Further additions to test.py

--- src/cinet/test.py
+++ src/cinet/test.py
@@ -1,7 +1,10 @@
 print("starting")
+import torch
 from cinet import *
+import numpy as np
 model = cinet()
 model.set_params()
 X = getCINETSampleInput()
 model.fit(X)
 py_model = model.getPytorchModel()
+test = np.array(X.iloc[4].values[2:], dtype=np.float16)
