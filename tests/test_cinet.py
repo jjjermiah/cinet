48f96b6f562d4085bba2685eb5b3ef099204b908
kevinTopGen <kevintabatabaei@gmail.com>
2022-08-23 21:11:53 +0000

    Modified tests

--- tests/test_cinet.py
+++ tests/test_cinet.py
@@ -1,4 +1,12 @@
 from cinet import *
 
-DCmodel = deepCINET()
-ECmodel = ECINET()
+def test_init_deepCINET():
+    """Test initializing DeepCINET"""
+    DCmodel = deepCINET()
+    return DCmodel
+
+def test_init_ECINET():
+    """Test initializing ECINET (Linear CINET)"""
+    ECmodel = ECINET(detla=0.05)
+    
+    assert (ECmodel.delta == 0.05), "Delta not initialized correctly"
